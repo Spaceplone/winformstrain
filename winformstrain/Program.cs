cda7cfbec15d835a588677fe8a5a039b235abfa3
Spaceplone <airplane-hugs@yandex.com>
2024-10-20 15:59:48 +0200

    domain based

--- winformstrain/Program.cs
+++ winformstrain/Program.cs
@@ -1,17 +1,30 @@
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+
 namespace winformstrain
 {
     internal static class Program
     {
+        public static IConfiguration Configuration { get; private set; }
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+
+            var currentDirectory = AppContext.BaseDirectory;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(currentDirectory) // Set the base path
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
+
+        private static void ConfigureServices(Servicecolle)
     }
 }
